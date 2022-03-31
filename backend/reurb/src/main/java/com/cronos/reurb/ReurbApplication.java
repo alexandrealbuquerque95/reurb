a94bc1b354904f8b4d2874ca76cd1d07d4987436
alexandrealbuquerque95 <alexandrealbuquerque95@hotmail.com>
2022-03-31 19:45:38 -0300

    Criação BackEnd

---
+++ backend/reurb/src/main/java/com/cronos/reurb/ReurbApplication.java
@@ -0,0 +1,13 @@
+package com.cronos.reurb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReurbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReurbApplication.class, args);
+	}
+
+}
---
+++ backend/reurb/src/test/java/com/cronos/reurb/ReurbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cronos.reurb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReurbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
