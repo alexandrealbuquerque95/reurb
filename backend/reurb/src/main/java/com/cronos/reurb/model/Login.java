3fa93a6592b7b95354fc9220f38dd6c83de1f066
alexandrealbuquerque95 <alexandrealbuquerque95@hotmail.com>
2022-04-02 01:47:26 -0300

    Alteração LoginController e Login Model - BackEnd

---
+++ backend/reurb/src/main/java/com/cronos/reurb/model/Login.java
@@ -0,0 +1,37 @@
+package com.cronos.reurb.model;
+
+public class Login 
+{
+	Integer id;
+	String nome;
+	Integer idade;
+	String endereco;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+
+}
